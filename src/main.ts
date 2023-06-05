c978f666a5990399e1e39b82e4788d4c51a1d1a4
alex-castillo95 <alexc@pickerexpress.com>
2023-06-05 14:07:08 -0500

    fixed body-parser

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    bodyParser.json({
-      limit: '50mb',
-      type: ['application/json', 'text/plain'],
-    }),
-  );
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
   app.enableCors({
